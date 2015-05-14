50236441e9c88956e7198d5e29274b57b02a202c
Femi Lasisi <lasisi28@gmail.com>
2015-05-14 11:28:15 +0100

    build 4

---
+++ app/leave/LeaveDeclinedCtrl.js
@@ -0,0 +1,18 @@
+var app = angular.module("hermes");
+
+var LeaveApprovedCtrl = function ($scope,$rootScope,$http,$log,$location) {
+
+    $rootScope.page = "leaveApproved";
+
+    $http.get("services/leaveAPI.php")
+        .success(function(data){
+            $scope.leave = data;
+
+            $scope.leaveApproved = _.where($scope.leave,{"status":"1"});
+        });
+    $scope.table = function(leave_id){
+        $location.url('/leave/review/'+leave_id);
+    };
+};
+
+app.controller("LeaveApprovedCtrl",LeaveApprovedCtrl);
